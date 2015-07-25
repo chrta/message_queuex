27461c0500b184a7b08c93f4059a074be4de281c
Christian Taedcke <hacking@taedcke.com>
2015-07-25 15:38:32 +0200

    Add a few typespecs.

--- lib/message_queue.ex
+++ lib/message_queue.ex
@@ -1,39 +1,43 @@
 defmodule MessageQueue do
-	  @on_load :init
+    @on_load :init
 
   def init() do
     :erlang.load_nif("./priv_dir/lib_elixir_mq", 0)
   end
 
+  @spec open(String.t) :: {atom, integer}
   def open(mq_file) do
     _open(String.to_char_list(mq_file))
   end
 
+  @spec open(integer) :: {atom, integer, bitstring}
   def read(fd) do
     _read(fd)
   end
 
+  @spec write(integer,integer, bitstring) :: atom
   def write(fd, priority, bin_data) do
     _write(fd, priority, bin_data)
   end
 
+  @spec close(integer) :: atom
   def close(fd) do
     _close(fd)
   end
 
   def _open(mq_file) do
-    "NIF library not loaded"
+    :erlang.nif_error("NIF library not loaded")
   end
 
   def _read(fd) do
-    "NIF library not loaded"
+    :erlang.nif_error("NIF library not loaded")
   end
 
   def _close(fd) do
-    "NIF library not loaded"
+    :erlang.nif_error("NIF library not loaded")
   end
 
   def _write(fd, priority, bin_data) do
-    "NIF library not loaded"
+    :erlang.nif_error("NIF library not loaded")
   end
 end
