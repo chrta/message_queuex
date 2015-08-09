9867560141571246811dc9a3a747473c4a3c1b82
Christian Taedcke <hacking@taedcke.com>
2015-08-09 09:47:45 +0200

    Indent cpp files and mive pid into MessageQueueNif class.

--- c_src/lib_elixir_mq.cpp
+++ c_src/lib_elixir_mq.cpp
@@ -14,228 +14,243 @@
 
 static boost::asio::io_service* io = nullptr;
 
-static ErlNifPid* dst = nullptr;
-
 class MessageQueueNif : public MessageQueue<MessageQueueNif>
 {
 public:
-MessageQueueNif(boost::asio::io_service& ioService, const std::string& queueName, OpenFlags flags)
-  : MessageQueue(ioService, queueName, flags)
-{}
+    MessageQueueNif(boost::asio::io_service& ioService, const std::string& queueName, OpenFlags flags, ErlNifPid pid)
+        : MessageQueue(ioService, queueName, flags)
+    {}
 
-void on_mq_data(const std::vector<uint8_t>& data, int priority)
-{
-    std::cout << "on_mq_data" << data.size() << " bytes, prio " << priority << std::endl;
-    if (!dst)
+    void on_mq_data(const std::vector<uint8_t>& data, int priority)
     {
-        return;
+        std::cout << "on_mq_data" << data.size() << " bytes, prio " << priority << std::endl;
+        ErlNifEnv* env = enif_alloc_env();
+
+        nifpp::binary bin_data(data.size());
+        std::copy(data.begin(), data.end(), bin_data.data);
+        nifpp::TERM message = nifpp::make(env, std::make_tuple(nifpp::make(env, nifpp::str_atom("ok")), nifpp::make(env, priority), nifpp::make(env, bin_data)));
+        enif_send(NULL, &owner, env, message);
+        enif_free_env(env);
     }
-    ErlNifEnv* env = enif_alloc_env();
 
-    nifpp::binary bin_data(data.size());
-    std::copy(data.begin(), data.end(), bin_data.data);
-    nifpp::TERM message = nifpp::make(env, std::make_tuple(nifpp::make(env, nifpp::str_atom("ok")), nifpp::make(env, priority), nifpp::make(env, bin_data)));
-    enif_send(NULL, dst, env, message);
-    enif_free_env(env);
-}
+    void on_mq_read_error(int error_number)
+    {
+        std::cout << "on_mq_read_error " << strerror(error_number) << std::endl;
+        //do not report read errors to the user
+    }
 
-void on_mq_read_error(int error_number)
-{
-    std::cout << "on_mq_read_error " << strerror(error_number) << std::endl;
-    //do not report read errors to the user
-}
+private:
+    ErlNifPid owner;
 };
 
 static MessageQueueNif* queue = nullptr;
 
 static ERL_NIF_TERM report_string_error(ErlNifEnv* env, const std::string& message)
 { 
-  return nifpp::make(env, std::make_tuple(nifpp::str_atom("error"), message));
+    return nifpp::make(env, std::make_tuple(nifpp::str_atom("error"), message));
 }
 
 static ERL_NIF_TERM report_errno_error(ErlNifEnv* env, int error_number)
 {
-  return report_string_error(env, strerror(error_number));
+    return report_string_error(env, strerror(error_number));
 }
 
 extern "C" ERL_NIF_TERM _open(ErlNifEnv* env, int /*arc*/, const ERL_NIF_TERM argv[])
 {
-  std::string path;
-  bool read_flag = false;
-  bool write_flag = false;
-  
-  ERL_NIF_TERM opts;
+    std::string path;
+    bool read_flag = false;
+    bool write_flag = false;
 
-  if (!nifpp::get(env, argv[0], path))
-  {
-    return enif_make_badarg(env);
-  }
-  
-  opts = argv[1];
+    ERL_NIF_TERM opts;
 
-  std::vector<nifpp::str_atom> open_flags;
-  if (!nifpp::get(env, opts, open_flags))
-  {
-    return enif_make_badarg(env);
-  }
-
-  for (auto atom : open_flags)
-  {
-      if (atom == "read")
-      {
-        read_flag = true;
-      }
-      else if (atom == "write")
-      {
-        write_flag = true;
-      }
-      else
-      {
+    if (!nifpp::get(env, argv[0], path))
+    {
         return enif_make_badarg(env);
-      }
-  }
+    }
 
-  MessageQueueNif::OpenFlags flags = MessageQueueNif::OpenFlags::INVALID;
+    opts = argv[1];
 
-  if (read_flag && write_flag)
-  {
-    flags = MessageQueueNif::OpenFlags::READ_WRITE;
-  }
-  else if (read_flag)
-  {
-    flags = MessageQueueNif::OpenFlags::READ_ONLY;
-  }
-  else if (write_flag)
-  {
-    flags = MessageQueueNif::OpenFlags::WRITE_ONLY;
-  }
+    std::vector<nifpp::str_atom> open_flags;
+    if (!nifpp::get(env, opts, open_flags))
+    {
+        return enif_make_badarg(env);
+    }
 
-  delete queue;
-  queue = nullptr;
-  delete dst;
-  dst = nullptr;
-  
-  try
-  {
-    queue = new MessageQueueNif{*io, path, flags};
-    dst = new ErlNifPid;
-    enif_self(env, dst);
-    //TODO delete ptr
+    for (auto atom : open_flags)
+    {
+        if (atom == "read")
+        {
+            read_flag = true;
+        }
+        else if (atom == "write")
+        {
+            write_flag = true;
+        }
+        else
+        {
+            return enif_make_badarg(env);
+        }
+    }
 
-    std::cout << "Open ok: " << queue->getId() << std::endl;
-    return nifpp::make(env, std::make_tuple(nifpp::str_atom("ok"), queue->getId()));
-  }
-  catch(...)
-  {
-    return report_errno_error(env, errno);
-  }  
+    MessageQueueNif::OpenFlags flags = MessageQueueNif::OpenFlags::INVALID;
+
+    if (read_flag && write_flag)
+    {
+        flags = MessageQueueNif::OpenFlags::READ_WRITE;
+    }
+    else if (read_flag)
+    {
+        flags = MessageQueueNif::OpenFlags::READ_ONLY;
+    }
+    else if (write_flag)
+    {
+        flags = MessageQueueNif::OpenFlags::WRITE_ONLY;
+    }
+
+    delete queue;
+    queue = nullptr;
+    ErlNifPid owner;
+    enif_self(env, &owner);
+    try
+    {
+        queue = new MessageQueueNif{*io, path, flags, owner};
+        //TODO delete ptr
+
+        std::cout << "Open ok: " << queue->getId() << std::endl;
+        return nifpp::make(env, std::make_tuple(nifpp::str_atom("ok"), queue->getId()));
+    }
+    catch(...)
+    {
+        return report_errno_error(env, errno);
+    }
 }
 
 extern "C" ERL_NIF_TERM _read(ErlNifEnv* env, int /*arc*/, const ERL_NIF_TERM argv[])
 {
 #if 0
-  mqd_t queueId = -1;
-  char buf[MAXBUFLEN];
-  ErlNifBinary r;
-  ssize_t res;
-  unsigned int prio = 0;
+    mqd_t queueId = -1;
+    char buf[MAXBUFLEN];
+    ErlNifBinary r;
+    ssize_t res;
+    unsigned int prio = 0;
 
-  enif_get_int(env, argv[0], &queueId);
-  res = mq_receive(queueId, buf, MAXBUFLEN, &prio);
+    enif_get_int(env, argv[0], &queueId);
+    res = mq_receive(queueId, buf, MAXBUFLEN, &prio);
 
-  if (res > 0)
-  {
-    enif_alloc_binary(res, &r);
-    memcpy(r.data, buf, res);
+    if (res > 0)
+    {
+        enif_alloc_binary(res, &r);
+        memcpy(r.data, buf, res);
 
-    ERL_NIF_TERM priority = enif_make_int(env, prio);
-    ERL_NIF_TERM status = enif_make_atom(env, "ok");
-    ERL_NIF_TERM data =  enif_make_binary(env, &r);
-    return enif_make_tuple3(env, status, priority, data);
-  }
-  else if (errno == EAGAIN)
-  {
-    enif_alloc_binary(0, &r);
-    ERL_NIF_TERM priority = enif_make_int(env, 0);
-    ERL_NIF_TERM status = enif_make_atom(env, "ok");
-    ERL_NIF_TERM data =  enif_make_binary(env, &r);
-    return enif_make_tuple3(env, status, priority, data);
-  }
+        ERL_NIF_TERM priority = enif_make_int(env, prio);
+        ERL_NIF_TERM status = enif_make_atom(env, "ok");
+        ERL_NIF_TERM data =  enif_make_binary(env, &r);
+        return enif_make_tuple3(env, status, priority, data);
+    }
+    else if (errno == EAGAIN)
+    {
+        enif_alloc_binary(0, &r);
+        ERL_NIF_TERM priority = enif_make_int(env, 0);
+        ERL_NIF_TERM status = enif_make_atom(env, "ok");
+        ERL_NIF_TERM data =  enif_make_binary(env, &r);
+        return enif_make_tuple3(env, status, priority, data);
+    }
 #endif
-  return report_errno_error(env, /*errno*/ 9);
+    return report_errno_error(env, /*errno*/ 9);
 }
 
 extern "C" ERL_NIF_TERM _write(ErlNifEnv* env, int /*arc*/, const ERL_NIF_TERM argv[])
 {
-  ErlNifBinary data;
-  int result;
-  mqd_t queue = -1;
-  unsigned int prio = 0;
+    if (!queue)
+    {
+        return report_errno_error(env, EBADF);
+    }
 
-  enif_get_int(env, argv[0], &queue);
-  enif_get_uint(env, argv[1], &prio);
-  if (!enif_inspect_binary(env, argv[2], &data))
-  {
-    return report_string_error(env, "Error converting given data into binary");
-  }
 
-  result = mq_send(queue, (char*) data.data, data.size, prio);
+    int priority = 0;
+    if (!nifpp::get(env, argv[1], priority))
+    {
+        return enif_make_badarg(env);
+    }
 
-  if (result == 0)
-  {
-    return enif_make_atom(env, "ok");
-  }
+    ErlNifBinary bin_data;
+    if (!nifpp::get(env, argv[2], bin_data))
+    {
+        return enif_make_badarg(env);
+    }
+    std::vector<uint8_t> data(bin_data.data, bin_data.data+ bin_data.size);
+    queue->write(data, priority);
+#if 0
+    ErlNifBinary data;
+    int result;
+    mqd_t queue = -1;
+    unsigned int prio = 0;
 
-  return report_errno_error(env, errno);
+    enif_get_int(env, argv[0], &queue);
+    enif_get_uint(env, argv[1], &prio);
+    if (!enif_inspect_binary(env, argv[2], &data))
+    {
+        return report_string_error(env, "Error converting given data into binary");
+    }
+
+    result = mq_send(queue, (char*) data.data, data.size, prio);
+
+    if (result == 0)
+    {
+        return enif_make_atom(env, "ok");
+    }
+
+    return report_errno_error(env, errno);
+#endif
 }
 
 
 extern "C" ERL_NIF_TERM _close(ErlNifEnv* env, int /*arc*/, const ERL_NIF_TERM argv[])
 {
-  mqd_t queueId = -1;
-  
-  enif_get_int(env, argv[0], &queueId);
-  try
-  {
-    auto temp = queue;
-    queue = nullptr;
-    delete temp;
-  }
-  catch(...)
-  {
-    return report_errno_error(env, errno);
-  }
-  return enif_make_atom(env, "ok");
-#if 0
-  if (mq_close(queueId) == 0)
-  {
+    mqd_t queueId = -1;
+
+    enif_get_int(env, argv[0], &queueId);
+    try
+    {
+        auto temp = queue;
+        queue = nullptr;
+        delete temp;
+    }
+    catch(...)
+    {
+        return report_errno_error(env, errno);
+    }
     return enif_make_atom(env, "ok");
-  }
-  return report_errno_error(env, errno);
+#if 0
+    if (mq_close(queueId) == 0)
+    {
+        return enif_make_atom(env, "ok");
+    }
+    return report_errno_error(env, errno);
 #endif
 }
 
 
 static void* thread_func(void* /*arg*/)
 {
-  try
-  {
-    boost::asio::io_service io_service;
-    io = &io_service;
-    //do not exit run() when running out of work
-    boost::asio::io_service::work work(io_service);
-    //Reader reader(ioService);
-    io_service.run();
+    try
+    {
+        boost::asio::io_service io_service;
+        io = &io_service;
+        //do not exit run() when running out of work
+        boost::asio::io_service::work work(io_service);
+        //Reader reader(ioService);
+        io_service.run();
+        io = nullptr;
+        std::cout << "Asio exit ok" << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
     io = nullptr;
-    std::cout << "Asio exit ok" << std::endl;
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-  io = nullptr;
-  
-  return nullptr;
+
+    return nullptr;
 }
 
 
@@ -244,37 +259,37 @@ static ErlNifTid tid;
 
 extern "C" int load(ErlNifEnv* /*env*/, void** /*priv*/, ERL_NIF_TERM /*load_info*/)
 {
-  thread_opts = enif_thread_opts_create(const_cast<char*>("thread_opts"));
+    thread_opts = enif_thread_opts_create(const_cast<char*>("thread_opts"));
 
-  if (enif_thread_create(const_cast<char*>(""), &tid, thread_func, nullptr, thread_opts) != 0)
-  {
-    return -1;
-  }
+    if (enif_thread_create(const_cast<char*>(""), &tid, thread_func, nullptr, thread_opts) != 0)
+    {
+        return -1;
+    }
 
-  return 0;
+    return 0;
 }
 
 extern "C" void unload(ErlNifEnv* /*env*/, void* /*priv*/)
 {
-  void* resp;
+    void* resp;
 
-  if (io)
-  {
-    io->stop();
-  }
+    if (io)
+    {
+        io->stop();
+    }
 
-  enif_thread_join(tid, &resp);
+    enif_thread_join(tid, &resp);
 
-  enif_thread_opts_destroy(thread_opts);
+    enif_thread_opts_destroy(thread_opts);
 }
 
 
 ErlNifFunc nif_funcs[] =
 {
-  {"_open",  2, _open,  0},
-  {"_read",  1, _read,  0},
-  {"_write", 3, _write, 0},
-  {"_close", 1, _close, 0}
+    {"_open",  2, _open,  0},
+    {"_read",  1, _read,  0},
+    {"_write", 3, _write, 0},
+    {"_close", 1, _close, 0}
 };
 
 extern "C"
--- c_src/message_queue.hpp
+++ c_src/message_queue.hpp
@@ -11,7 +11,7 @@ constexpr int MAXBUFLEN = 1024;
 
 struct message
 {
-  int i;
+    int i;
 };
 
 template <typename T>
@@ -19,127 +19,156 @@ class MessageQueue
 {
 public:
 
-  enum class OpenFlags {READ_ONLY, WRITE_ONLY, READ_WRITE, INVALID};
-  
-  MessageQueue(boost::asio::io_service& ioService, const std::string& queueName, OpenFlags flags)
-    : ioService(ioService),
-      streamDescriptor(ioService),
-      timer(ioService),
-      mqid(-1)
-  {
-    if (flags == OpenFlags::READ_ONLY)
+    enum class OpenFlags {READ_ONLY, WRITE_ONLY, READ_WRITE, INVALID};
+
+    MessageQueue(boost::asio::io_service& ioService, const std::string& queueName, OpenFlags flags)
+        : ioService(ioService),
+          streamDescriptor(ioService),
+          timer(ioService),
+          mqid(-1)
     {
-      mqid = mq_open(queueName.c_str(), O_RDONLY | O_NONBLOCK);
-    }
-    else
-    {
-      int open_flags = O_CREAT | O_NONBLOCK;
-      struct mq_attr mattr;
-      mattr.mq_maxmsg = 10;
-      mattr.mq_msgsize = sizeof(struct message);
+        if (flags == OpenFlags::READ_ONLY)
+        {
+            mqid = mq_open(queueName.c_str(), O_RDONLY | O_NONBLOCK);
+        }
+        else
+        {
+            int open_flags = O_CREAT | O_NONBLOCK;
+            struct mq_attr mattr;
+            mattr.mq_maxmsg = 10;
+            mattr.mq_msgsize = sizeof(struct message);
 
-      if (flags == OpenFlags::WRITE_ONLY)
-      {
-	open_flags |= O_WRONLY;
-      }
-      else if (flags == OpenFlags::READ_WRITE)
-      {
-	open_flags |= O_RDWR;
-      }
-      
-      mqid = mq_open(queueName.c_str(), open_flags, S_IREAD | S_IWRITE, &mattr);
-    }
+            if (flags == OpenFlags::WRITE_ONLY)
+            {
+                open_flags |= O_WRONLY;
+            }
+            else if (flags == OpenFlags::READ_WRITE)
+            {
+                open_flags |= O_RDWR;
+            }
 
-    std::cout << "message queue mqid = " << mqid << std::endl;
+            mqid = mq_open(queueName.c_str(), open_flags, S_IREAD | S_IWRITE, &mattr);
+        }
 
-    if (mqid == -1)
-    {
-      std::cerr << "Failed to open queue with error: " << strerror(errno) << std::endl;
-      throw std::runtime_error("Failed to open queue");
-    }
-    
-    streamDescriptor.assign(mqid);
-#if 0    
-    streamDescriptor.async_write_some(
-				      boost::asio::null_buffers(),
-				      boost::bind(&MessageQueue::handleWrite,
-						this,
-						boost::asio::placeholders::error,
-						boost::asio::placeholders::bytes_transferred));
-#endif
+        std::cout << "message queue mqid = " << mqid << std::endl;
 
-    streamDescriptor.async_read_some(
-				     boost::asio::null_buffers(),
-				     boost::bind(&MessageQueue::handleRead,
-						 this,
-						 boost::asio::placeholders::error)); 
-  }
+        if (mqid == -1)
+        {
+            std::cerr << "Failed to open queue with error: " << strerror(errno) << std::endl;
+            throw std::runtime_error("Failed to open queue");
+        }
 
-  ~MessageQueue()
-  {
-    if (mqid >= 0)
-    {
-      mq_close(mqid);
-    }
-  }
-  
-  void handleWrite(const boost::system::error_code &ec, std::size_t bytes_transferred)
-  {
-    timer.expires_from_now(boost::posix_time::microseconds(100));
-    timer.async_wait(boost::bind(&MessageQueue::handleTimer, this,
-			       boost::asio::placeholders::error));
-  }
-  
-  void handleTimer(const boost::system::error_code& ec)
-  {
-    message msg;
-    int sendRet = mq_send(mqid, (const char*)&msg, sizeof(message), 0);
-    std::cout << "sendRet = " << sendRet << std::endl;
-    
-    streamDescriptor.async_write_some(
-				      boost::asio::null_buffers(),
-				      boost::bind(&MessageQueue::handleWrite,
-						  this,
-						  boost::asio::placeholders::error,
-						  boost::asio::placeholders::bytes_transferred));
-  }
+        streamDescriptor.assign(mqid);
 
-  void handleRead(const boost::system::error_code &ec)
-  {
-    if (ec)
-    {
-      static_cast<T*>(this)->on_mq_read_error(ec.value());
-      return;
+        streamDescriptor.async_write_some(
+                    boost::asio::null_buffers(),
+                    boost::bind(&MessageQueue::handleWrite,
+                                this,
+                                boost::asio::placeholders::error,
+                                boost::asio::placeholders::bytes_transferred));
+
+        streamDescriptor.async_read_some(
+                    boost::asio::null_buffers(),
+                    boost::bind(&MessageQueue::handleRead,
+                                this,
+                                boost::asio::placeholders::error));
     }
 
-    char buf[MAXBUFLEN];
-    unsigned int prio = 0;
-    
-    int res = mq_receive(mqid, buf, MAXBUFLEN, &prio);
-    std::cout << "RX " << res << " bytes from " << mqid << std::endl;
-    if (res < 0)
+    ~MessageQueue()
     {
-      std::cout << "Errno is " << strerror(errno) << std::endl;
+        //TODO handle it in the io service, otherwise race condition
+        do_close();
     }
-    if (res > 0)
-    {
-      std::vector<uint8_t> data(buf, buf + res);
-      static_cast<T*>(this)->on_mq_data(data, prio);
-    }
-    else
-    {
-      static_cast<T*>(this)->on_mq_read_error(errno);
-    }
-  }
 
-  mqd_t getId() const
-  {
-    return mqid;
-  }
-  
+    void handleWrite(const boost::system::error_code &ec, std::size_t bytes_transferred)
+    {
+        timer.expires_from_now(boost::posix_time::microseconds(100));
+        timer.async_wait(boost::bind(&MessageQueue::handleTimer, this,
+                                     boost::asio::placeholders::error));
+    }
+
+    void handleTimer(const boost::system::error_code& ec)
+    {
+        message msg;
+        int sendRet = mq_send(mqid, (const char*)&msg, sizeof(message), 0);
+        std::cout << "sendRet = " << sendRet << std::endl;
+
+        streamDescriptor.async_write_some(
+                    boost::asio::null_buffers(),
+                    boost::bind(&MessageQueue::handleWrite,
+                                this,
+                                boost::asio::placeholders::error,
+                                boost::asio::placeholders::bytes_transferred));
+    }
+
+    void handleRead(const boost::system::error_code &ec)
+    {
+        if (ec)
+        {
+            static_cast<T*>(this)->on_mq_read_error(ec.value());
+            return;
+        }
+
+        char buf[MAXBUFLEN];
+        unsigned int prio = 0;
+
+        int res = mq_receive(mqid, buf, MAXBUFLEN, &prio);
+        std::cout << "RX " << res << " bytes from " << mqid << std::endl;
+        if (res < 0)
+        {
+            std::cout << "Errno is " << strerror(errno) << std::endl;
+        }
+        if (res > 0)
+        {
+            std::vector<uint8_t> data(buf, buf + res);
+            static_cast<T*>(this)->on_mq_data(data, prio);
+        }
+        else
+        {
+            static_cast<T*>(this)->on_mq_read_error(errno);
+        }
+    }
+
+    mqd_t getId() const
+    {
+        return mqid;
+    }
+
+    void write(const std::vector<uint8_t> data, int priority)
+    {
+        //ioService.post(boost::bind(&MessageQueue::do_write, this, data, priority));
+        streamDescriptor.async_write_some(
+                    boost::asio::null_buffers(),
+                    boost::bind(&MessageQueue::handleWrite,
+                                this,
+                                boost::asio::placeholders::error,
+                                boost::asio::placeholders::bytes_transferred));
+    }
+
+    void close()
+    {
+        ioService.post(boost::bind(&MessageQueue::do_close, this));
+    }
+
 private:
-  boost::asio::io_service& ioService;
-  boost::asio::posix::stream_descriptor streamDescriptor;
-  boost::asio::deadline_timer timer;
-  mqd_t mqid;
+
+    void do_write(const boost::system::error_code &ec, const std::vector<uint8_t> data, int priority)
+    {
+        std::cout << "Do write " << data.size() << " bytes with prio " << priority << std::endl;
+    }
+
+    void do_close()
+    {
+        if (mqid >= 0)
+        {
+            mq_close(mqid);
+            mqid = -1;
+        }
+    }
+
+
+    boost::asio::io_service& ioService;
+    boost::asio::posix::stream_descriptor streamDescriptor;
+    boost::asio::deadline_timer timer;
+    mqd_t mqid;
 };
